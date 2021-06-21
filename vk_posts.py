081e822b7f592b1820fc0eea6e39ecb0fbd438fa
muckas <motobike8@outlook.com>
2021-06-21 19:28:16 +0500

    Handle long messages the right way

--- vk_posts.py
+++ vk_posts.py
@@ -1,13 +1,13 @@
 import logging
+import telegram
 
 log = logging.getLogger()
 
-def send(tg, user_id, post, poster_name, domain, feed_id):
+def send(tg, user_id, post, poster_name, domain, feed_id, post_id):
   msg_text = ''
   msg_attachments = []
   post_id = post['id']
-  post_text = post['text']
-  msg_text = post_text + '\n----------------------------\n' + poster_name + '\nhttps://vk.com/' + domain
+  msg_text = post['text']
   if 'attachments' in post.keys():
     attachments = post['attachments']
     for attachment in attachments:
@@ -17,7 +17,9 @@ def send(tg, user_id, post, poster_name, domain, feed_id):
         msg_attachments.append(photo['url'])
   if len(msg_attachments) > 1:
     if len(msg_text) > 1024:
-      msg_text = msg_text[:-100] + '...\n* В посте слишком много текста для отправки в Telegram *'
+      msg_text = msg_text[:800] 
+      msg_text += '...\n* В посте слишком много текста для отправки в Telegram *'
+    msg_text += '\n----------------------------\n' + poster_name + f'\nhttps://vk.com/wall{feed_id}_{post_id}'
     msg_photos = []
     for photo in msg_attachments:
       if photo is msg_attachments[0]:
@@ -27,9 +29,12 @@ def send(tg, user_id, post, poster_name, domain, feed_id):
     tg.sendMediaGroup(chat_id=user_id, media=msg_photos)
   elif len(msg_attachments) == 1:
     if len(msg_text) > 1024:
-      msg_text = msg_text[:-100] + '...\n* В посте слишком много текста для отправки в Telegram *'
+      msg_text = msg_text[:900] 
+      msg_text += '...\n* В посте слишком много текста для отправки в Telegram *'
+    msg_text += '\n----------------------------\n' + poster_name + f'\nhttps://vk.com/wall{feed_id}_{post_id}'
     tg.send_photo(chat_id=user_id, photo=msg_attachments[0], caption=msg_text)
   else:
     if len(msg_text) > 4096:
-      msg_text = msg_text[:-100] + '...\n* В посте слишком много текста для отправки в Telegram *'
+      msg_text = msg_text[:900] + '...\n* В посте слишком много текста для отправки в Telegram *'
+    msg_text += '\n----------------------------\n' + poster_name + f'\nhttps://vk.com/wall{feed_id}_{post_id}'
     tg.send_message(chat_id=user_id, text=msg_text)
