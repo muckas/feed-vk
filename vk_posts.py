b5cd9a9ba17fa5028ba689581449e172804aa1cc
muckas <motobike8@outlook.com>
2021-06-22 14:16:46 +0500

    Handle link attachments

--- vk_posts.py
+++ vk_posts.py
@@ -28,6 +28,21 @@ def get_playlist(attachment):
   count = attachment['audio_playlist']['count']
   return f'{title} - {count} аудио'
 
+def get_links(attachment, playlists, articles, links):
+  if attachment['link']['description'] == 'Playlist':
+    title = attachment['link']['title']
+    url = attachment['link']['url']
+    playlists.append(f'Музыкальный плейлист: {title}\n{url}')
+  elif attachment['link']['description'] == 'Article':
+    title = attachment['link']['title']
+    url = attachment['link']['url']
+    articles.append(f'Статья: {title}\n{url}')
+  else:
+    title = attachment['link']['title']
+    url = attachment['link']['url']
+    links.append(f'Ссылка: {title}\n{url}')
+  return playlists, articles, links,
+
 def get_post(vk, post, poster_name, domain, feed_id):
   post_id = post['id']
   post_full = ''
@@ -50,6 +65,8 @@ def get_post(vk, post, poster_name, domain, feed_id):
   post_videos = []
   post_audios = []
   post_playlists = []
+  post_articles = []
+  post_links = []
   post_gifs = []
   post_docs = []
   if 'attachments' in post.keys():
@@ -62,24 +79,36 @@ def get_post(vk, post, poster_name, domain, feed_id):
       elif attachment['type'] == 'audio':
         post_audios.append(get_audio(attachment))
       elif attachment['type'] == 'audio_playlist':
-        log.info('Playlist')
         post_playlists.append(get_playlist(attachment))
+      elif attachment['type'] == 'link':
+        post_playlists, post_articles, post_links = get_links(attachment,
+                                                            post_playlists,
+                                                            post_articles,
+                                                            post_links
+                                                            )
       elif attachment['type'] == 'doc':
         doc = get_doc(attachment)
         if doc[0] == 'gif':
           post_gifs.append(doc[1])
         else:
           post_files.append(doc[1])
+  gif_count = len(post_gifs)
   if len(post_photos) != 0 and len(post_gifs) != 0:
     post_gifs = []
+    post_full += '\n' + f'В посте ещё {gif_count} gif'
   if len(post_gifs) > 1:
     post_gifs = [post_gifs[0]]
+    post_full += '\n' + f'В посте ещё {gif_count-1} gif'
   for video in post_videos:
     post_full += f'\nВидео: {video}'
   for audio in post_audios:
     post_full += f'\nАудио: {audio}'
   for playlist in post_playlists:
-    post_full += f'\nПлэйлист: {playlist}'
+    post_full += '\n' + playlist
+  for article in post_articles:
+    post_full += '\n' + article
+  for link in post_links:
+    post_full += '\n' + link
   for doc in post_docs:
     post_full += f'\nДокумент: {doc}'
   post_msg = post_full + post_bottom_text
